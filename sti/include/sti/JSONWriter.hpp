40818bf51864a5ced476db77461f86c5105f3b81
Baptiste Dupy <baptiste.dupy@gmail.com>
2014-09-10 15:21:42 +0200

    Correct Headers Infos

--- sti/include/sti/JSONWriter.hpp
+++ sti/include/sti/JSONWriter.hpp
@@ -1,10 +1,31 @@
-//
-//  JSONWriter.hpp
-//  STIG
-//
-//  Created by Baptiste Dupy on 09/09/14.
-//
-//
+///
+///  JSONWriter.hpp
+///  is part of STI Library.
+///
+///  Static Type Info - A C++ Static Reflexion Library based on Clang.
+///  Get more infos on STIG at <https://github.com/trfd/STI>
+///
+///  Created by Baptiste Dupy on 08/09/2014.
+///  Contact:
+///          Mail:       <baptiste.dupy@gmail.com>
+///          GitHub:     trfd <https://github.com/trfd>
+///          Website:    <http://www.trf-d.com>
+///
+///  Copyright (C) 2014 Baptiste (trfd) Dupy
+///
+///  This program is free software: you can redistribute it and/or modify
+///  it under the terms of the GNU General Public License as published by
+///  the Free Software Foundation, either version 3 of the License, or
+///  (at your option) any later version.
+///
+///  This program is distributed in the hope that it will be useful,
+///  but WITHOUT ANY WARRANTY; without even the implied warranty of
+///  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///  GNU General Public License for more details.
+///
+///  You should have received a copy of the GNU General Public License
+///  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
 
 #ifndef STIG_JSONWriter_hpp
 #define STIG_JSONWriter_hpp
