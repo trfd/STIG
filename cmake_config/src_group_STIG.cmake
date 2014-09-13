2f93046c8e621406f4d0470dad9944a74fcfd55e
Baptiste Dupy <baptiste.dupy@gmail.com>
2014-09-13 18:20:09 +0200

    Split CES into seperate project

--- cmake_config/src_group_STIG.cmake
+++ cmake_config/src_group_STIG.cmake
@@ -4,12 +4,6 @@ SOURCE_GROUP("stig\\include" FILES ${stig_include})
 FILE(GLOB stig_include_stig ${SOURCE_GROUP_PREFIX}/stig/include/stig/*)
 SOURCE_GROUP("stig\\include\\stig" FILES ${stig_include_stig})
 
-FILE(GLOB stig_include_stig_ces ${SOURCE_GROUP_PREFIX}/stig/include/stig/ces/*)
-SOURCE_GROUP("stig\\include\\stig\\ces" FILES ${stig_include_stig_ces})
-
 FILE(GLOB stig_src ${SOURCE_GROUP_PREFIX}/stig/src/*)
 SOURCE_GROUP("stig\\src" FILES ${stig_src})
-
-FILE(GLOB stig_src_swig ${SOURCE_GROUP_PREFIX}/stig/src/swig/*)
-SOURCE_GROUP("stig\\src\\swig" FILES ${stig_src_swig})
 
