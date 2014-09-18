196dee05e856f02b4803cbf2ef04b958c2ddf73a
trfd <baptiste.dupy@gmail.com>
2014-09-18 18:41:43 +0200

    Squashed 'CES/' changes from f7d1a21..fd7e06e
    fd7e06e Delete version
    489c152 Delete runcmake
    3af8185 Update .gitignore
    a1a5a71 Update README.md
    075fad9 Update README.md
    2625eca Update README.md
    49d5777 Update README.md
    c5a0d2f Update README.md
    c042661 Update README.md
    bc4496f Update README.md
    0639c01 Update README.md
    5b6e743 Merge pull request #1 from trfd/devt
    38c6833 CES Functionnal
    7b0332c Add AST class
    510835d CMake file updated
    e90c5e9 Split CES into seperate project
    
    git-subtree-dir: CES
    git-subtree-split: fd7e06e2c56ee81dff63542f56b54725e99b76db

---
+++ ces/include/ces/CESIO.hpp
@@ -0,0 +1,95 @@
+///
+///  CESIO.hpp
+///  is part of CES Library.
+///
+///  C++ Embedded Script - CES is used to write template file for code generation.
+///  More information about CES at <https://github.com/trfd/CES>
+///
+///  Created by Baptiste Dupy on 12/09/2014.
+///  Contact:
+///          Mail:       <baptiste.dupy@gmail.com>
+///          GitHub:     trfd <https://github.com/trfd>
+///          Website:    <http://www.trf-d.com>
+///
+///  Copyright (C) 2014 Baptiste (trfd) Dupy
+///
+///  This program is free software: you can redistribute it and/or modify
+///  it under the terms of the GNU General Public License as published by
+///  the Free Software Foundation, either version 3 of the License, or
+///  (at your option) any later version.
+///
+///  This program is distributed in the hope that it will be useful,
+///  but WITHOUT ANY WARRANTY; without even the implied warranty of
+///  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///  GNU General Public License for more details.
+///
+///  You should have received a copy of the GNU General Public License
+///  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef STIG_CESIO_hpp
+#define STIG_CESIO_hpp
+
+#include <map>
+
+#include "Singleton.hpp"
+
+#include "InterpreterSession.hpp"
+
+namespace ces
+{
+    /// <summary>
+    /// I/O interface for Lua / CES
+    /// </summary>
+    /// This class register interpreter session
+    /// and forward CES.out call from lua to
+    /// corresponding interpreter sessions
+    class IO : public ck::utils::Singleton<IO>
+    {
+    public:
+        
+        typedef InterpreterSession              Session;
+        typedef std::map<unsigned,Session*>     SessionMap;
+        typedef SessionMap::iterator            SessionMap_it;
+        
+        inline static IO* instance(){return ck::utils::Singleton<IO>::instance();}
+        
+        /// <summary>
+        /// Registers an interpreter sessions
+        /// </summary>
+        /// When IO receive an out call with the session's id
+        /// IO forward output to the session.
+        static void registerSession(Session* s_)
+        {
+            instance()->m_sessions[s_->id] = s_;
+        }
+        
+        /// <summary>
+        /// Unregisters an interpreter sessions
+        /// </summary>
+        static void unregisterSession(Session* s_)
+        {
+            instance()->m_sessions.erase(s_->id);
+        }
+        
+        /// <summary>
+        /// Forward a CES.out call from lua to interpreter session that matches id.
+        /// </summary>
+        inline static void out(unsigned sessionID_,const char* str_)
+        {
+            SessionMap_it it = instance()->m_sessions.find(sessionID_);
+            
+            if(it != instance()->m_sessions.end())
+                it->second->out(str_);
+        }
+        
+    private:
+        
+        
+        /// <summary>
+        /// Map of all registered session by ID.
+        /// </summary>
+        SessionMap m_sessions;
+    };
+}
+
+#endif
