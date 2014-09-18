179aea589a51f070c66291ff15578580c0897c17
trfd <baptiste.dupy@gmail.com>
2014-09-18 20:35:58 +0200

    Cleaning

--- devt/source/dev_paths.hpp
+++ devt/source/dev_paths.hpp
@@ -8,7 +8,6 @@
 
 // Root directory of the Cytok Project (absolute)
 // (Dev Only)
-<<<<<<< HEAD
 #define STIG_PROJECT_ROOT_DIR ""
 
 // Location of the libraries in the Cytok Project (absolute)
@@ -22,18 +21,3 @@
 // Location of the dev test directory in the Cytok Project (absolute)
 // (Dev Only)
 #define STIG_DEV_TEST_DIR ""
-=======
-#define STIG_PROJECT_ROOT_DIR "/Users/Baba/dev/trfd/STIG"
-
-// Location of the libraries in the Cytok Project (absolute)
-// (Dev only)
-#define STIG_LIB_DIR "/Users/Baba/dev/trfd/STIG/builds/lib"
-
-// Location of the header in the Cytok Project (absolute)
-// (Dev Only)
-#define STIG_INCLUDE_DIR "/Users/Baba/dev/trfd/STIG/stig/include/"
-
-// Location of the dev test directory in the Cytok Project (absolute)
-// (Dev Only)
-#define STIG_DEV_TEST_DIR "/Users/Baba/dev/trfd/STIG/devt/"
->>>>>>> 576c8d8d79d2f22a09c43438fc4b58d2e2e33d2a
