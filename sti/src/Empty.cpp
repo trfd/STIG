4697c1bd9442a4745a4a3ff13eaa379e3391d827
trfd <baptiste.dupy@gmail.com>
2014-09-18 19:33:01 +0200

    Squashed 'STI/' content from commit 576c8d8
    git-subtree-dir: STI
    git-subtree-split: 576c8d8d79d2f22a09c43438fc4b58d2e2e33d2a

---
+++ sti/src/Empty.cpp
@@ -0,0 +1,11 @@
+//
+//  Empty.cpp
+//  STIG
+//
+//  Created by Baptiste Dupy on 09/09/14.
+//
+
+#include "sti/DeclNode.hpp"
+#include "sti/JSONWriter.hpp"
+#include "sti/NodeSerialization.hpp"
+#include "sti/Serialization.hpp"
