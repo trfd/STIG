1684f57acae340eff35e0cd68ce177a1204e3400
Baptiste Dupy <baptiste.dupy@gmail.com>
2015-07-15 13:55:22 +0200

    Cleanup Code Base

--- CES/cmake_config/install-gtest.sh
+++
@@ -1,7 +0,0 @@
-wd=`pwd`
-apt-get install -y libgtest-dev
-cd /usr/src/gtest
-cmake CMakeLists.txt
-make
-cp *.a /usr/lib
-cd $wd
--- cmake_config/config.hpp
+++
@@ -1,7 +0,0 @@
-//
-//  config.hpp
-//  STIG
-//
-//  Created by Baptiste Dupy on 03/07/2014.
-//
-//
